7dc62382285a096fe9c95db546ecaf3e3fffed6c
Foxtrot72 <155225676+Foxtrot72@users.noreply.github.com>
2024-06-14 10:04:11 +0200

    Aggiunta main solo per istanziare gli oggetti

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
 public class Main {
-    
+  public static void main(String[] args) {
+    Email email=new Email(null, null, null);
+    NotificaApp notificaApp=new NotificaApp(null, null, null);
+    Sms sms=new Sms(null, null, 2);
+
+
+  }   
 }
